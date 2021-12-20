14570eb5798890a3319494d980e4c407c4980973
Sumett ampornsak <644230035@webmail.npru.ac.th.com>
2021-12-20 15:49:50 +0700

    second comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("Hello IT");
+        System.out.println("HelloSumett");
+        System.out.println("HelloAmpornsak");
+        
     }
 }
